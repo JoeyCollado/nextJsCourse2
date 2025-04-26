57b00ebc3b8894900f57ef80ea4a41e232d79a97
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-04-26 17:10:22 +0800

    private-pages-and-route-groups

---
+++ private-pages-and-route-groups/src/app/(auth)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
+
+//after putting parenthesis in the folder name we can directly access the sub routes register and login withouth auth prefix
---
+++ private-pages-and-route-groups/src/app/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
+
+
+//private pages
+//to make a page privae just add _ as a prefix to whatever the folder's name
+
+
+//after putting parenthesis in the folder name we can directly access the sub routes register and login withouth auth prefix
