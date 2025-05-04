eee33a814ab2be6aa1e050e65013f81a9734c6f4
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-04 14:27:31 +0800

    movie project

--- movie-web/src/app/movies/[id]/page.tsx
+++ movie-web/src/app/movies/[id]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaStar } from 'react-icons/fa';
 
 interface MovieProps{
     params: {
@@ -43,6 +44,37 @@ const page = ({params}: MovieProps) => {
         <h2 className='text-4xl font-semibold text-white mb-4'>{movie?.title}</h2>
         <p className='text-lg text-gray-300'>{movie?.description}</p>
       </div>
+
+      {/* rating / comments summary */}
+      <div className='bg-gray-800 p-6 rounded-xl shadow-lg'>
+        <div className='flex justify-between items-center mb-6'>
+          <div>
+            <p className='text-xl text-gray-300'>Rating</p>
+            <div className='flex items-center text-yellow-400'>
+              <FaStar className='mr-1'/>
+              <span className='text-2xl font-bold'>{movie?.rating}</span> / 5
+            </div>
+          </div>
+
+          <div>
+             <p className='text-xl text-gray-300'>Comments:</p>
+             <p className='text-2xl font-semibold text-white'>{movie?.comments}</p>
+          </div>
+
+        </div>
+      </div>
+{/* comment section */}
+      <div>
+          <h3 className='text-3xl font-semibold text-white mb-4'>Comments</h3>
+          <ul className='space-y-4'>
+            {movie?.commentList.map((comment, index) => (
+              <li key={index} className='bg-gray-800 p-4 rounded-lg hover:shadow-xl transition-shadow duration-300'>
+                <p className='text-gray-300'>{comment}</p>
+              </li>
+            ))}
+          </ul>
+      </div>
+
     </div>
   )
 }
