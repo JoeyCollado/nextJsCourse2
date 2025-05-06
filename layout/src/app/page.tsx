7e9ff407de34c57770f8fe733dd7fa9454bf3c88
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-06 11:49:57 +0800

    layout

---
+++ layout/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default page
+
+
+//layout file = acts as a wrapper that surrounds your page content.
