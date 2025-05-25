4c03524fef3162b203e520b4855526900435f61b
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-25 12:34:04 +0800

    fonts

---
+++ middleware/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
+
+//middleware = something that happens in the middle of request
