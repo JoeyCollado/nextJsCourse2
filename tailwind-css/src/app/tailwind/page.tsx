52803fb851d5fd632269a3c64bacf8167f0e4075
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-29 11:43:35 +0800

    tailwind done

--- tailwind-css/src/app/tailwind/page.tsx
+++ tailwind-css/src/app/tailwind/page.tsx
@@ -4,14 +4,17 @@ const page = () => {
   return (
     <div>
       <section>
-        <div>
-            <h1>Hello nextjs</h1>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
+        <div className='bg-teal-700 flex justify-between p-4 m-4 '>
+            <h1 className='font-bold'>Hello nextjs</h1>
+            <ul className='flex space-x-5 '>
+                <li className='cursor-pointer hover:scale-105'>Home</li>
+                <li className='cursor-pointer hover:scale-105'>About</li>
+                <li className='cursor-pointer hover:scale-105'>Contact</li>
             </ul>
         </div>
+
+        <h1 className='text-center font-bold text-2xl mt-10'>Sample Talwind Styling</h1>
+        <p className='container m-4 font-light text-center'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum distinctio delectus quod! Id quibusdam perspiciatis assumenda obcaecati alias illo porro ex reiciendis debitis iusto, voluptatem iste eius saepe eaque omnis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum corrupti dolor aliquid necessitatibus labore architecto enim suscipit optio tempore molestiae repellendus assumenda aperiam, eius voluptate commodi consequatur, veritatis sint nisi.</p>
       </section>
     </div>
   )
