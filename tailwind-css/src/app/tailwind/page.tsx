40a067b01ceb9914ec6d8c102631068cefd87112
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-29 11:34:50 +0800

    basic nav

---
+++ tailwind-css/src/app/tailwind/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <section>
+        <div>
+            <h1>Hello nextjs</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default page
