39a5ef2988928f6bbacc526aeac16b528094117d
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-31 22:03:26 +0800

    shadcn

--- http-methods/src/app/movies/route.ts
+++ http-methods/src/app/movies/route.ts
@@ -17,4 +17,66 @@ export async function POST(req: Request){ //posting the movie
    return new Response(JSON.stringify(newMovie));
 }
 
- 
+export async function PATCH(
+    req: Request,
+    { params }: { params: { id: string } }
+  ) {
+    const { id } = params;
+    const movieId = +id;
+  
+    // Check if movie exists
+    const movie = movies.find((m) => m.id === movieId);
+  
+    if (!movie) {
+      return new Response(JSON.stringify({ error: "Movie not found" }), {
+        status: 404,
+      });
+    }
+  
+    try {
+      const updatedMovie = await req.json();
+  
+      // Find the index of the movie
+      const index = movies.findIndex((m) => m.id === movieId);
+  
+      if (index === -1) {
+        return new Response(JSON.stringify({ error: "Movie not found" }), {
+          status: 404,
+        });
+      }
+  
+      // Update the movie
+      movies[index] = { ...movie, ...updatedMovie };
+  
+      return new Response(JSON.stringify(movies[index]), { status: 200 });
+    } catch (error) {
+      return new Response(JSON.stringify({ error: "Failed to parse JSON" }), {
+        status: 400,
+      });
+    }
+  }
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: { id: string } }
+  ) {
+    const { id } = params;
+    const movieId = +id;
+  
+    // Find the movie index
+    const index = movies.findIndex((m) => m.id === movieId);
+  
+    if (index === -1) {
+      return new Response(JSON.stringify({ error: "Movie not found" }), {
+        status: 404,
+      });
+    }
+  
+    // Remove the movie from the collection
+    movies.splice(index, 1);
+  
+    return new Response(
+      JSON.stringify({ message: "Movie deleted successfully" }),
+      { status: 200 }
+    );
+  }
---
+++ shadcn/src/app/hello-world/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./page.module.css"
+
+const page = () => {
+  return (
+    <div className={styles.title}>
+      Hello World
+    </div>
+  )
+}
+
+export default page
+
+//page.module.css = here we can define predefined styles that is reusable to any component
