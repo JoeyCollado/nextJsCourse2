e2b696f6b05610316391c1af43c9595098e4f048
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-02 12:21:33 +0800

    magic-ui

---
+++ magic-ui/src/app/page.tsx
@@ -0,0 +1,22 @@
+import { Bebas_Neue } from "next/font/google";
+
+
+const bebasFont = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Bebas Neue</h1>
+      <p className={`${bebasFont.className}`}>This is some random text.</p>
+
+     
+    </main>
+  );
+};
+
+export default Home;
+
+// magic ui = open source animated component library
