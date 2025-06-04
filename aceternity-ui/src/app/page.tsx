1a1fa61e0d6bf0232a1b78adea247aab141788f3
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-04 10:27:19 +0800

    3d card effect

--- aceternity-ui/src/app/page.tsx
+++ aceternity-ui/src/app/page.tsx
@@ -11,3 +11,8 @@ const page = () => {
 export default page
 
 //aceternity ui = one of the best component library
+//npx shadcn@latest init
+//zinc
+
+//3d card component = npx shadcn@latest add https://ui.aceternity.com/registry/3d-card.json
+
