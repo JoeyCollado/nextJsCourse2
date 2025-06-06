0f7f91a204805303cea42ab7798495f2dc1be51e
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-06 19:23:28 +0800

    Rendering parallel routes

--- parallelroutes/src/app/page.tsx
+++ parallelroutes/src/app/page.tsx
@@ -11,3 +11,4 @@ const page = () => {
 export default page
 
 //to create a parallel route = simply prefix the folder name with @
+//parallel routes = are special components that allows us to use the loading.tsx and also error.tsx template and many more
