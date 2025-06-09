bf8641c971f23f0591a4b0afd6b24bc6095b1696
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-09 18:12:28 +0800

    defining sidebar parallel route and rendering sidebar component

---
+++ music-player/src/app/musics/layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+const MusicLayout = ({children, sidebar}: {children: ReactNode, sidebar: ReactNode}) => {
+  return (
+    <div className='flex'>
+      {sidebar}
+      <div></div>
+      {children }
+    </div>
+  )
+}
+
+export default MusicLayout
--- music-player/src/app/musics/page.tsx
+++ music-player/src/app/musics/page.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 const page = () => {
-  return null
-  
+  return <></>
+
 }
 
 export default page
