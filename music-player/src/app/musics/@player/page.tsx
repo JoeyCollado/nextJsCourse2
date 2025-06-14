61c3f8a2db5f2720d9feef8e831101ce7645cf9e
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-14 20:07:28 +0800

    player progress bar

--- music-player/src/app/musics/@player/page.tsx
+++ music-player/src/app/musics/@player/page.tsx
@@ -25,6 +25,17 @@ const Player = () => {
           className="cursor-pointer hover:text-white transition-colors"
         />
       </div>
+
+      <div className="flex items-center gap-4 text-gray-400">
+        <span>2:21</span>
+
+        <div className="relative w-64 h-1 bg-gray-700 rounded-md">
+           <div className="absolute top-0 left-0 h-1 bg-white rounded-md" style={{width: "50%"}}/>
+
+           <input type="range" className="absolute top-0 left-0 w-full h-1 bg-transparent cursor-pointer" title="progress bar"/>
+        </div>
+        <span>3:11</span>
+      </div>
     </div>
   );
 };
