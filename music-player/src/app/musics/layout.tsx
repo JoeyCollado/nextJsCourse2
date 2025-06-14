c069f8a82901cb4ea8a8bb1f157a359938e337d3
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-14 19:55:54 +0800

    Default.tsx for player route and layout.tsx config

--- music-player/src/app/musics/layout.tsx
+++ music-player/src/app/musics/layout.tsx
@@ -4,15 +4,17 @@ const MusicLayout = ({
   children,
   sidebar,
   songs,
+  player,
 }: {
   children: ReactNode;
   sidebar: ReactNode;
   songs: ReactNode;
+  player: ReactNode;
 }) => {
   return (
     <div className="flex">
       {sidebar}
-      <div className="flex-1 p-6w">{songs}</div>
+      <div className="flex-1 p-6w">{player} {songs}</div>
       {children}
     </div>
   );
