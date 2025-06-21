00643bf7f20b4a2ec34380a03e4e9986d5490459
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-21 22:05:45 +0800

    React icons

--- games-project/src/app/games/page.tsx
+++ games-project/src/app/games/page.tsx
@@ -1,9 +1,11 @@
+import Sidebar from '@/components/Sidebar'
 import React from 'react'
 
 const Games = () => {
   return (
     <div>
       {/* Sidebar */}
+      <Sidebar/>
       <div className="flex-1 bg-gray-100 p-5">
         <h1 className="text-2xl font-bold mb-4">Featured Games</h1>
         <div className="flex gap-4">
--- games-project/src/components/Sidebar.tsx
+++ games-project/src/components/Sidebar.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <div>
-      
+      <h2>Gaming Dashboard</h2>
+      <div>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+      </div>
     </div>
   )
 }
