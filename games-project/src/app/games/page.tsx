8d65e7b4ac961707dbf633229f7b725f27cbcc1e
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-21 22:04:09 +0800

    components

--- games-project/src/app/games/page.tsx
+++ games-project/src/app/games/page.tsx
@@ -4,6 +4,12 @@ const Games = () => {
   return (
     <div>
       {/* Sidebar */}
+      <div className="flex-1 bg-gray-100 p-5">
+        <h1 className="text-2xl font-bold mb-4">Featured Games</h1>
+        <div className="flex gap-4">
+            {/* game card */}
+        </div>
+      </div>
     </div>
   )
 }
