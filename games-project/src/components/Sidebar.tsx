cd5cc95264eaa797100a69f543136f8d463af499
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-21 22:07:31 +0800

    sidebar comp ui-1

--- games-project/src/components/Sidebar.tsx
+++ games-project/src/components/Sidebar.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link'
 import React from 'react'
+import { FaGamepad } from 'react-icons/fa'
 
 const Sidebar = () => {
   return (
@@ -7,9 +9,22 @@ const Sidebar = () => {
       <div>
         <ul>
             <li>
-                
+                <FaGamepad className='mr-2'/>Games
+            </li>
+            <li>
+                <FaGamepad className='mr-2'/>Games
+            </li>
+            <li>
+                <FaGamepad className='mr-2'/>Games
+            </li>
+            <li>
+                <FaGamepad className='mr-2'/>Games
             </li>
         </ul>
+
+        <section className='mb-[7rem]'>
+            <Link href='/login' className='text-2xl font-bold'>Login</Link>
+        </section>
       </div>
     </div>
   )
