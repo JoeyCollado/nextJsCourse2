840e39f4c055c64b43597c760a76d32f45cdb475
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-22 17:56:12 +0800

    defining intercepted routes for games route to login route

---
+++ games-project/src/app/games/(..)login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
+
+
+//we defined two dots because the login route is outside the games route
--- games-project/src/app/login/page.tsx
+++ games-project/src/app/login/page.tsx
@@ -4,7 +4,7 @@ const LoginPage = () => {
   return (
     <div className='flex items-center justify-center min-h-screen bg-gray-100'>
       <div className='w-full max-w-md p-6 bg-white rounded-lg shadow-md'>
-        <h2 className='text-2xl font-semibold mb-4'>Login</h2>
+        <h2 className='text-2xl font-semibold mb-4 text-black text-center'>Login</h2>
         <form>
             <div>
                 <label className='block text-sm font-medium text-gray-700'>Email</label>
@@ -14,7 +14,7 @@ const LoginPage = () => {
                 <label className='block text-sm font-medium text-gray-700'>Password</label>
                 <input className='w-full px-3 py-2 mt-1 border rounded-lg focus:outline-none focus:ring focus:border-blue-300' type='password' placeholder='Enter your password'></input>
             </div>
-        <button className='w-full px-4 py-2 text-white bg-gray-900 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300' type='submit'>Login</button>
+        <button className='w-full px-4 py-2 text-white bg-gray-900 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 mt-[1rem]' type='submit'>Login</button>
         </form>
       </div>
     </div>
