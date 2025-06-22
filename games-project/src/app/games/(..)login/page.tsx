ef23640a145ca727412c3be0ef7267b9aba0a0ae
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-22 18:00:56 +0800

    login intercepted route ui

--- games-project/src/app/games/(..)login/page.tsx
+++ games-project/src/app/games/(..)login/page.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-const page = () => {
+const LoginPage = () => {
+
+    const router = useRouter();
+    const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <div>
-      
-    </div>
+    <>
+      {isOpen && (
+        <div>
+            <div>
+                <button>&#1005;</button>
+            </div>
+
+            <h2>Login</h2>
+            <form>
+                <div>
+                    <label>Email</label>
+                    <input type="email" placeholder="Enter your email"></input>
+                </div>
+
+                <div>
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter your password"></input>
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+      )}
+   </>
   )
 }
 
-export default page
+export default LoginPage
 
 
 //we defined two dots because the login route is outside the games route
