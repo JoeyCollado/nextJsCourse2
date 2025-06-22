9ca640070ed2c9eabc9c40b674e13ccd7616a253
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-22 17:41:07 +0800

    annotating and destructuring game props

--- games-project/src/components/GameCard.tsx
+++ games-project/src/components/GameCard.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const GameCard = () => {
+type GameProps = {
+  game: {
+    id: string;
+    name: string;
+    img: string
+  }
+}
+
+
+const GameCard = ({game}: GameProps) => {
   return (
     <div>
-      
+      <img src={game.img} alt={game.name}/>
     </div>
   )
 }
