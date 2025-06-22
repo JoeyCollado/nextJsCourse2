80800dc169c7b6895fcbdfa490466452278e112d
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-22 17:52:09 +0800

    styling login page

--- games-project/src/app/login/page.tsx
+++ games-project/src/app/login/page.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div className=''>
-      <div>
-        <h2>Login</h2>
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <div className='w-full max-w-md p-6 bg-white rounded-lg shadow-md'>
+        <h2 className='text-2xl font-semibold mb-4'>Login</h2>
         <form>
             <div>
-                <label>Email</label>
-                <input type='email' placeholder='Enter your email'></input>
+                <label className='block text-sm font-medium text-gray-700'>Email</label>
+                <input className='w-full px-3 py-2 mt-1 border rounded-lg focus:outline-none focus:ring focus:border-blue-300' type='email' placeholder='Enter your email'></input>
             </div>
             <div>
-                <label>Password</label>
-                <input type='email' placeholder='Enter your email'></input>
+                <label className='block text-sm font-medium text-gray-700'>Password</label>
+                <input className='w-full px-3 py-2 mt-1 border rounded-lg focus:outline-none focus:ring focus:border-blue-300' type='password' placeholder='Enter your password'></input>
             </div>
-            <button type='submit'>Login</button>
+        <button className='w-full px-4 py-2 text-white bg-gray-900 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300' type='submit'>Login</button>
         </form>
       </div>
     </div>
