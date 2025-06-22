7960b6778fdbad25da8e734c4d0df15b2714212b
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-22 17:47:47 +0800

    login ui

--- games-project/src/app/login/page.tsx
+++ games-project/src/app/login/page.tsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>
-      
+    <div className=''>
+      <div>
+        <h2>Login</h2>
+        <form>
+            <div>
+                <label>Email</label>
+                <input type='email' placeholder='Enter your email'></input>
+            </div>
+            <div>
+                <label>Password</label>
+                <input type='email' placeholder='Enter your email'></input>
+            </div>
+            <button type='submit'>Login</button>
+        </form>
+      </div>
     </div>
   )
 }
