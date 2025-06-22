c190e3390dbe52b3ff7c5f1c5fa5cb1c7e2d8be6
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-22 17:42:36 +0800

    gamecard ui

--- games-project/src/components/GameCard.tsx
+++ games-project/src/components/GameCard.tsx
@@ -11,8 +11,9 @@ type GameProps = {
 
 const GameCard = ({game}: GameProps) => {
   return (
-    <div>
-      <img src={game.img} alt={game.name}/>
+    <div className='bg-gray-800 text-white p-4 rounded-lg shadow-md'>
+      <img src={game.img} alt={game.name} className='w-full h-32 object-cover rounded-md'/>
+      <h3 className='mt-2 text-lg font-semibold'>{game.name}</h3>
     </div>
   )
 }
