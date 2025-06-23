d62ce2a8edf0de427daadeec01c94c3dff70b5b8
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-23 16:24:21 +0800

    parallel intercepting done

--- parallel-intercepting-refactored/src/app/page.tsx
+++ parallel-intercepting-refactored/src/app/page.tsx
@@ -12,3 +12,9 @@ export default page
 
 //parallel interecepting real power = instead of rendering entire page ui, you can just render a pop up model
 // parallel route + intercepting route
+//if you want to use parallel route with intercepting route
+//first define parallel route
+//seondly define intercepting route inside parallel route
+//thirdly define the main route inside the intercepting route
+//lastly define default.tsx inside intercepting route
+//lastly define layout.tsx and just register your route inside it
