b04a83abe7b33df07ffd9e3705209ab5aa99eda2
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-23 16:08:18 +0800

    parallel intercepting real power

---
+++ parallel-intercepting-refactored/src/app/ecommerce/(.)[product]/page.tsx
@@ -0,0 +1,31 @@
+import { products } from "@/db/data";
+
+const InterceptedProduct = async ({
+    params,
+  }: {
+    params: { product: string };
+  }) => {
+    const { product } = await params;
+  
+    const findProduct = products.find((f) => f.id === +product);
+  
+    return (
+      <>
+        <div className="fixed inset-0 flex items-center justify-center bg-black/80   z-50">
+          <div className="bg-white p-8 rounded-lg shadow-lg text-center relative">
+            <h1 className="text-3xl mb-4">{findProduct?.name}</h1>
+  
+            <img
+              src={findProduct?.image}
+              alt={findProduct?.name}
+              className="w-[40rem] mx-auto rounded-lg"
+            />
+  
+            <p className="text-2xl mt-4">Price: {findProduct?.price}</p>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  export default InterceptedProduct;
---
+++ parallel-intercepting-refactored/src/app/ecommerce/[product]/page.tsx
@@ -0,0 +1,24 @@
+import { products } from "@/db/data";
+
+const Product = async ({ params }: { params: { product: string } }) => {
+  const { product } = await params;
+
+  const findProduct = products.find((p) => p.id === +product);
+
+
+  return (
+    <div className="m-[4rem]">
+      <h1 className="text-3xl mb-[1rem]">{findProduct?.name}</h1>
+
+      <img
+       className="w-[40rem]"
+       src={findProduct?.image}
+       alt={findProduct?.name}
+      />
+
+<p className="text-2xl mt-[1rem]">Price: {findProduct?.price}</p>
+    </div>
+  );
+};
+
+export default Product;
