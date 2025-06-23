ff2b67d4adc70711d9016d3fd64665b00cfbfde5
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-23 16:15:17 +0800

    defining parallel route and defining intercepted route inside it

--- parallel-intercepting-refactored/src/app/ecommerce/layout.tsx
+++ parallel-intercepting-refactored/src/app/ecommerce/layout.tsx
@@ -3,7 +3,8 @@ import React, { ReactNode } from 'react'
 const EcommerceLAyout = ({children , product}: {children: ReactNode, product:ReactNode}) => { //destructing and annotataing
   return (
     <div>
-      
+      {children}
+      {product}
     </div>
   )
 }
