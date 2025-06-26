9aade6f768818944943540155b224feb037ae9f8
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-26 20:41:51 +0800

    server comp done

--- server/src/app/page.tsx
+++ server/src/app/page.tsx
@@ -13,24 +13,4 @@ const page = () => {
 export default page
 
 
-// server = component by default, no interactivity
-// client = can use hooks like useState, has interactivity
-
-//how to use
-//to use server, do nothing
-//tp use client, put "use client"; at the top of the file
-
-//when to use
-//Server
-//great for data fetching
-//backend
-//private/confidential info
-//complex rendering logic
-//SEO - friendly dynamic content
-
-//Client
-//Interactivity
-//State management
-//browser API's
-//hooks
-//client side animation and effects
+//server component = by default is what we use in nextjs
