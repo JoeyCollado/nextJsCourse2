f9dc966953ec6fe77c9bfbd5f22f647f1b0fa2e0
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-27 21:42:20 +0800

    client

---
+++ client/src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+
+      
+    </div>
+  )
+}
+
+export default page
+
+
+//server component = by default is what we use in nextjs
