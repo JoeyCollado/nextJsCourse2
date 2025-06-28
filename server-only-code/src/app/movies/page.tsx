12b4bda78759d09c540c8838d321d45316dae3bc
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-28 16:34:13 +0800

    server-only-code

---
+++ server-only-code/src/app/movies/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+
+    console.log("this message is coming from the server component");
+
+  return (
+    <div>
+      Movies
+    </div>
+  )
+}
+
+export default page
---
+++ server-only-code/src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React, { useState } from 'react'
+
+const Movies = () => {
+
+  // const [movies, setMovies] = useState([]);
+  console.log("This message is coming from client component")
+
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default Movies
+
+
+//server component = by default is what we use in nextjs
