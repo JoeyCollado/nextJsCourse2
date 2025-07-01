7bfc55c535c7a143ca5917eb5d337240c1a32885
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-07-01 14:49:42 +0800

    done

--- parent-effecting-children/src/app/dashboard/page.tsx
+++ parent-effecting-children/src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import Password from '@/components/Password'
 import Profile from '@/components/Profile'
 import Settings from '@/components/Settings'
 import React from 'react'
@@ -7,8 +9,12 @@ const page = () => {
     <div>
       <Settings/>
       <Profile/>
+      <Password/>
     </div>
   )
 }
 
 export default page
+
+//the parent component will affect the nested/children component
+//in this example the components i imported are in default server component but by making this route a client component all children components imported was the converted to client
