4ab399253f96e2f8be4f33b31595c90b017447c8
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-07-01 14:37:13 +0800

    client-only-code

---
+++ client-only-code/src/utils/index.ts
@@ -0,0 +1,9 @@
+//codes here only run on the server side
+
+import 'server-only';
+
+export const add = (a:number, b:number) => a + b;
+
+export const subtract = (a:number, b:number) => a - b;
+
+//npm i server-only = package that makes any code usable in server only
