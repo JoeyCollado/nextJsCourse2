e1419063da59d32c5b364d6f9ff259a305b28762
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-07-01 14:47:15 +0800

    dashboard route

---
+++ parent-effecting-children/src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Profile from '@/components/Profile'
+import Settings from '@/components/Settings'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Settings/>
+      <Profile/>
+    </div>
+  )
+}
+
+export default page
