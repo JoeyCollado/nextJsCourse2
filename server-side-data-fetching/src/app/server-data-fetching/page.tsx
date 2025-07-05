b1d4d0e5ea14f40405df45741b4dd2cc17951843
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-07-05 21:21:16 +0800

    Creating server component to fetch data

---
+++ server-side-data-fetching/src/app/server-data-fetching/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ToDoComponent = async () => {
+
+    await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ToDoComponent
