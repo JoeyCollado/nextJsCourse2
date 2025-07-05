b48d31b4b6381e0943dda1ca665841d5922f5e51
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-07-05 21:49:45 +0800

    server-data-fetching-data-done

--- server-side-data-fetching/src/app/server-data-fetching/page.tsx
+++ server-side-data-fetching/src/app/server-data-fetching/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ToDoComponent = async () => {
 
-    await fetch('https://jsonplaceholder.typicode.com/todos/1')
+   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+   const data = await response.json();
+   console.log(data);
   return (
     <div>
       
