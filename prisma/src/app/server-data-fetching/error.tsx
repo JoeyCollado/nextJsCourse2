008ed408f071bfa10094fd3308c313f2efb488b5
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-07-17 20:58:46 +0800

    prisma

---
+++ prisma/src/app/server-data-fetching/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+      you got Error!
+    </div>
+  )
+}
+
+export default Error
---
+++ prisma/src/app/server-data-fetching/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ToDoComponent = async () => {
+
+   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+   const data = await response.json();
+   console.log(data);
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.completed ? "Completed" : "Not Completed"}</p>
+    </div>
+  )
+}
+
+export default ToDoComponent
