d53256136412d89cf15b6db091fbe9a0aa69036d
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-08-15 15:00:03 +0800

    Successfully create a movie entry

--- prisma-setup/src/index.ts
+++ prisma-setup/src/index.ts
@@ -2,8 +2,24 @@ import { PrismaClient } from "../generated/prisma";
 
 const prisma = new PrismaClient() //instance of prisma client
 
+//function to create a movie
+async function createMovie(){
+   const newMovie = await prisma.movie.create({
+     data: {
+        title: 'Inception',
+        description: 'A cinematic masterpiece that seamlessly blends reality and dreams, Inception is a captivating story of a dream within a dream',
+        genre: "Sci-Fi",
+        releaseDate: new Date('2010-07-16'),
+        rating: 8.8,
+
+     }
+    }) //accessing our prisma and movie schema and performing create query
+    console.log(newMovie);
+}
+
 async function main(){//function to allow us to run our queries
     // C.R.U.D
+    await createMovie(); //executing createMovie function
 }
 
 //call function
@@ -12,3 +28,5 @@ main().then(async () => await prisma.$disconnect()).catch(async e => {
     await prisma.$disconnect();
     process.exit(1)
 })
+
+//npx ts-node .\src\index.ts = running index.ts file
