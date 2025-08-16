319b776dfd74bbfaec0bde49839eac1783dc0d81
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-08-16 15:27:31 +0800

    create multi movies

---
+++ create-multiple-movies/src/index.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient() //instance of prisma client
+
+//function to create a movie
+async function createMovie(){
+   const newMovie = await prisma.movie.create({
+     data: {
+        title: 'Inception2',
+        description: 'A cinematic masterpiece that seamlessly blends reality and dreams, Inception is a captivating story of a dream within a dream',
+        genre: "Sci-Fi",
+        releaseDate: new Date('2010-07-16'),
+        rating: 8.8,
+
+     }
+    }) //accessing our prisma and movie schema and performing create query, adding specific data to our database
+    console.log(newMovie);
+}
+
+async function main(){//function to allow us to run our queries
+    // C.R.U.D
+    await createMovie(); //executing createMovie function
+}
+
+//call function
+main().then(async () => await prisma.$disconnect()).catch(async e => {
+    console.log(e);
+    await prisma.$disconnect();
+    process.exit(1)
+})
+
+//npx ts-node .\src\index.ts = running index.ts file
