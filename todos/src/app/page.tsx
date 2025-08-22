5e89ea75e25009cac815c05ef577eaed8cb7ab65
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-08-22 12:04:12 +0800

    installing pacakge npx prisma init --datasource-provider sqlite

--- todos/src/app/page.tsx
+++ todos/src/app/page.tsx
@@ -11,3 +11,5 @@ const page = () => {
 export default page
 
 //prisma install = npm i -D prisma
+//prisma initialization and telling prisma what database are we using = npx prisma init --datasource-provider
+//npx prisma init --datasource-provider sqlite
