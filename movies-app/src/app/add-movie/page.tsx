24510cd8bd7a63d5749a8679a42c7ced6487eb52
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-03 11:00:40 +0800

    add movie ui phase 1

--- movies-app/src/app/add-movie/page.tsx
+++ movies-app/src/app/add-movie/page.tsx
@@ -1,9 +1,18 @@
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      
+      <h1>Add a new movie</h1>
+      <form>
+        <Label htmlFor='title'>Title</Label>
+        <Input type='text' name='title' required/>
+
+        <Label htmlFor='description'>Description</Label>
+        <Input name='description' required/>
+      </form>
     </div>
   )
 }
