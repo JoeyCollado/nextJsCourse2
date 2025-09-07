bea071aff07e3225f5cf5b116165d90f5349733f
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-07 13:14:48 +0800

    zod nested object done

--- zod-nestedObjects/src/app/page.tsx
+++ zod-nestedObjects/src/app/page.tsx
@@ -3,20 +3,24 @@ import {email, z} from 'zod'
 const page = () => {
   
 //zod schema (Shape)
-const userSchema = z.object({
+const userSchema = z.object({ //object
     name: z.string(),
-    age: z.number().optional(), //not required
-    email: z.string().email().default('default email'), //no value specified take the default value instead   
+    address: z.object({ //nested object
+      city: z.string(),
+      zipCode: z.string(),
+    }),
 })
-//error validator
-const result = userSchema.safeParse({
-  name: 'Joey',
-  age: 20, 
-  email: "invalid"
-})
-if(!result.success){
-  console.log(result.error.format());
+//data
+const user ={
+  name: "joey",
+  address: {
+    city: "Random city",
+    zipCode: "123"
+  }
 }
+//display
+console.log(userSchema.parse(user));
+
 
   return (
     <div>
