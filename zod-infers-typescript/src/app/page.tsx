7de13c8e0010e8ba8a92455851a60f4ab39cfb7f
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-10 11:20:35 +0800

    infer typescript with zod done

--- zod-infers-typescript/src/app/page.tsx
+++ zod-infers-typescript/src/app/page.tsx
@@ -2,10 +2,27 @@ import React from 'react'
 import {email, z} from 'zod'
 const page = () => {
   
-const roleSchema = z.enum(["admin", "user", "guest"]); //defining array
-const userRole = 'admin'
-console.log(roleSchema.parse(userRole));
-//enums
+const userSchema = z.object({ //defining schema
+  name: z.string(),
+  age: z.number(),
+  email: z.string().email(),
+})
+
+//creating different type based on schema
+type User = z.infer<typeof userSchema>; //this code means we all want the type from the user schema to be inside User
+
+const validUser: User = { //using the infer type schema User to create a validated data
+  name: "joey",
+  age: 22,
+  email: "joey123@gmail.com"
+}
+
+try{
+  const parsedUser = userSchema.parse(validUser);
+  console.log('Valid User: ',parsedUser);
+}catch(error){
+  console.log('Error: ', error)
+}
   return (
     <div>
       
