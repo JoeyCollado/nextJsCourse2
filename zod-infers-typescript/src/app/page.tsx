f1cd4b59c61ca059e10530c2af3ad19cf9203e87
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-10 11:02:37 +0800

    infer typescript

---
+++ zod-infers-typescript/src/app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {email, z} from 'zod'
+const page = () => {
+  
+const roleSchema = z.enum(["admin", "user", "guest"]); //defining array
+const userRole = 'admin'
+console.log(roleSchema.parse(userRole));
+//enums
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
+
+//how to install 
+//npm i zod
