7417b6e2b64ed32e630c45d63210f724adfacb7e
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-11 12:02:36 +0800

    sign up ui, and styled

--- zod-form-validation/src/app/sign-up/page.tsx
+++ zod-form-validation/src/app/sign-up/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className='flex min-h-screen items-center justify-center bg-gray-100'>
+      <Card className='w-full max-w-md p-6 shadow-lg'>
+        <CardHeader>
+            <CardTitle className='text-center text-xl font-semibold'>Sign up</CardTitle>
+        </CardHeader>
+      </Card>
     </div>
   )
 }
