4e14d397956f90b4a17006a3cde3c8cb5aad8be2
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-12 11:06:48 +0800

    formfield 1 done

--- zod-form-validation/src/app/sign-up/page.tsx
+++ zod-form-validation/src/app/sign-up/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import React from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { useForm } from 'react-hook-form'
 
 const page = () => {
+
+  const form = useForm({
+    defaultValues:{
+      name: '',
+      email: "",
+      password: ""
+    }
+  })
+
   return (
     <div className='flex min-h-screen items-center justify-center bg-gray-100'>
       <Card className='w-full max-w-md p-6 shadow-lg'>
@@ -11,7 +23,7 @@ const page = () => {
             <CardTitle className='text-center text-xl font-semibold'>Sign up</CardTitle>
         </CardHeader>
         <CardContent>
-          <Form>
+          <Form {...form}>
             <form className='space-y-4'>
               <div>
                 <FormField name='name' render={(filed) => (
